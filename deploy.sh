91a97d43bbf2d4f07e27127a8d21aed9a529769b
starinje <jared.starin@alphapoint.com>
2019-05-13 10:23:33 -0400

    adding travis configs

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo ${TRAVIS_JOB_NUMBER}
+echo ${TRAVIS_TAG}
+echo "tagging docker image"
+VERSION=`grep '"version":' package.json | sed 's/.*: "//' | sed 's/",//'`
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin ${DOCKER_REGISTRY}
+docker tag score-calculation-service ${DOCKER_REGISTRY}/score-calculation-service:${VERSION}
+docker tag score-calculation-service ${DOCKER_REGISTRY}/score-calculation-service:latest
+echo "pushing docker image"
+docker push ${DOCKER_REGISTRY}/score-calculation-service:${VERSION}
+docker push ${DOCKER_REGISTRY}/score-calculation-service:latest
