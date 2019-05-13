bc2763829440e48fcc4840b20e13d0f28b1bf66f
starinje <jared.starin@alphapoint.com>
2019-05-13 11:21:18 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,8 @@ echo ${TRAVIS_TAG}
 echo "tagging docker image"
 VERSION=`grep '"version":' package.json | sed 's/.*: "//' | sed 's/",//'`
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin ${DOCKER_REGISTRY}
-docker tag score-calculation-service ${DOCKER_REGISTRY}/score-calculation-service:${VERSION}
-docker tag score-calculation-service ${DOCKER_REGISTRY}/score-calculation-service:latest
+docker tag score-calculation-service ${DOCKER_REGISTRY}/${DOCKER_USERNAME}/score-calculation-service:${VERSION}
+docker tag score-calculation-service ${DOCKER_REGISTRY}/${DOCKER_USERNAME}/score-calculation-service:latest
 echo "pushing docker image"
-docker push ${DOCKER_REGISTRY}/score-calculation-service:${VERSION}
-docker push ${DOCKER_REGISTRY}/score-calculation-service:latest
-
-
+docker push ${DOCKER_REGISTRY}/${DOCKER_USERNAME}/score-calculation-service:${VERSION}
+docker push ${DOCKER_REGISTRY}/${DOCKER_USERNAME}/score-calculation-service:latest
