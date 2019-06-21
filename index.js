72ec34eebc4557da01d0e74b8e00dcc2e8e9c8c0
Jared Starin <starinje@gmail.com>
2019-06-21 08:11:11 -0400

    formatting

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
-const config = require("./config.js")
-const DataServiceClient = require("./library/dataServiceClient.js")
+const config = require("./config.js");
+const DataServiceClient = require("./lib/dataServiceClient.js");
 
+const FitnessDeviceClient = require("./lib/fitnessDeviceClient");
+const HealthScoreServiceClient = require("./lib/healthScoreServiceClient");
 
-const FitnessDeviceClient = require('./library/fitnessDeviceClient')
-const HealthScoreServiceClient = require('./library/healthScoreServiceClient')
-
-const dataServiceClient = new DataServiceClient(config)
-const fitnessDeviceClient = new FitnessDeviceClient(config)
-const healthScoreServiceClient = new HealthScoreServiceClient(config)
-
+const dataServiceClient = new DataServiceClient(config);
+const fitnessDeviceClient = new FitnessDeviceClient(config);
+const healthScoreServiceClient = new HealthScoreServiceClient(config);
 
 const main = async () => {
-    try{
-        console.log('generating health score for all accounts')
-        let accounts = await dataServiceClient.getAccounts()
+  try {
+    console.log("generating health score for all accounts");
+    let accounts = await dataServiceClient.getAccounts();
 
-        console.log(`Fetched ${accounts.length} accounts from data-service:`)
-        console.log(accounts)
+    console.log(`Fetched ${accounts.length} accounts from data-service:`);
+    console.log(accounts);
 
-        for (let account of accounts){
-            console.log('')
-            let fitnessDeviceData = await fitnessDeviceClient.getData(account)
+    for (let account of accounts) {
+      console.log("");
+      let fitnessDeviceData = await fitnessDeviceClient.getData(account);
 
-            console.log(`calculating health score for ${account.firstName} ${account.lastName} from fitness data`)
-            let healthScore = await healthScoreServiceClient.getHealthScore(fitnessDeviceData)
+      console.log(
+        `calculating health score for ${account.firstName} ${
+          account.lastName
+        } from fitness data`
+      );
+      let healthScore = await healthScoreServiceClient.getHealthScore(
+        fitnessDeviceData
+      );
 
-            console.log(`user ${account.firstName} ${account.lastName} has a calculated health score of ${healthScore}`)
-            console.log(`posting ${account.firstName} ${account.lastName}'s data to data-service`)
+      console.log(
+        `user ${account.firstName} ${
+          account.lastName
+        } has a calculated health score of ${healthScore}`
+      );
+      console.log(
+        `posting ${account.firstName} ${
+          account.lastName
+        }'s data to data-service`
+      );
 
-            await dataServiceClient.updateHealthScore(account, healthScore)
-
-        }
-    
-        
-
-    } catch (error){
-        console.log(error)
-    } finally{
-        setTimeout(() => {main()}, config.runInterval)
+      await dataServiceClient.updateHealthScore(account, healthScore);
     }
-}
+  } catch (error) {
+    console.log(error);
+  } finally {
+    setTimeout(() => {
+      main();
+    }, config.runInterval);
+  }
+};
 
-main()
+main();
