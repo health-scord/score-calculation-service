72516deee58ba3c15eb4a1e99cd80af9e62ecc1a
Jared Starin <starinje@gmail.com>
2019-06-21 08:22:27 -0400

    pointing to server

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 const defaults = {
-    RUN_INTERVAL:3000
-}
+  RUN_INTERVAL: 3000,
+  DATA_SERVICE_ENDPOINT: `http://68.183.100.145:5000`
+};
 
 let config = {
-    runInterval: process.env.RUN_INTERVAL ? process.env.RUN_INTERVAL : defaults.RUN_INTERVAL
-}
+  runInterval: process.env.RUN_INTERVAL
+    ? process.env.RUN_INTERVAL
+    : defaults.RUN_INTERVAL,
+  dataServiceEndpoint: process.env.RUN_INTERVAL
+    ? process.env.RUN_INTERVAL
+    : defaults.RUN_INTERVAL
+};
 
-
-
-
-
-module.exports = config
+module.exports = config;
