452aa273357099c784fc21b791ea194856c93644
Jared Starin <starinje@gmail.com>
2019-06-26 07:59:17 -0400

    qweqweqe

--- config.js
+++ config.js
@@ -1,15 +1,19 @@
 const defaults = {
   RUN_INTERVAL: 30000,
-  DATA_SERVICE_ENDPOINT: `http://68.183.100.145:5000`
+  DATA_SERVICE_PORT: `9000`,
+  DATA_SERVICE_URI: `data-service`
 };
 
 let config = {
   runInterval: process.env.RUN_INTERVAL
     ? process.env.RUN_INTERVAL
     : defaults.RUN_INTERVAL,
-  dataServiceEndpoint: process.env.DATA_SERVICE_ENDPOINT
-    ? process.env.DATA_SERVICE_ENDPOINT
-    : defaults.DATA_SERVICE_ENDPOINT
+  dataServiceUri: process.env.DATA_SERVICE_URI
+    ? process.env.DATA_SERVICE_URI
+    : defaults.DATA_SERVICE_URI,
+  dataServicePort: process.env.DATA_SERVICE_PORT
+    ? process.env.DATA_SERVICE_PORT
+    : defaults.DATA_SERVICE_PORT
 };
 
 module.exports = config;
